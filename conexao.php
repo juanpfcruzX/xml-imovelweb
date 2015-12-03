32b8fc00c81866af82257e1fd142028b760b6f2c
Daniel Carvalho <daniel.a.carvalho@outlook.com>
2015-12-03 14:50:44 -0300

    Atualizando XML com novas tags

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$db = mysql_connect("http://cinataimoveis.com.br", "cinataim_user", "z9#BhdhA)rx9") or die(mysql_error());
+
+$banco = mysql_select_db("cinataim_cinata_db", $db) or die(mysql_error());
+
+
+
+?>
